24ed7237a1d418e0220054141bafe8e9ee672b03
Brittney Kenady <bk244917@ohio.edu>
2019-01-23 16:36:40 -0500

    Testing

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Brittney Kenady
+ *        Date: 01/23/2019
+ * Description: N/A
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello, Git!" << endl;
+    return 0;
+}// main
